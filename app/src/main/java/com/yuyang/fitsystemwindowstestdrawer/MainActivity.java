914e74fae0b72a646cfaa323c86ebc7eaf8bf4d5
yuyang <272999712@qq.com>
2016-06-02 23:16:56 +0800

    代码整理

--- app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/MainActivity.java
+++ app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,29 +15,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
-import com.yuyang.fitsystemwindowstestdrawer.MetricsAbout.MetricsActivity;
-import com.yuyang.fitsystemwindowstestdrawer.androidLAnimationAbout.OptionsCompatActivity;
-import com.yuyang.fitsystemwindowstestdrawer.animationAbout.AnimationActivity;
-import com.yuyang.fitsystemwindowstestdrawer.cardViewPager.CardViewPagerActivity;
-import com.yuyang.fitsystemwindowstestdrawer.coordinatorLayoutAbout.MaterialDesignActivity;
-import com.yuyang.fitsystemwindowstestdrawer.customView.CustomActivity;
-import com.yuyang.fitsystemwindowstestdrawer.destWidget.DestWidgetActivity;
-import com.yuyang.fitsystemwindowstestdrawer.googleWidget.GoogleWidgetActivity;
-import com.yuyang.fitsystemwindowstestdrawer.horizontalFling.HorizontalFlingActivity;
-import com.yuyang.fitsystemwindowstestdrawer.internetAbout.manager.ConnectivityStateActivity;
-import com.yuyang.fitsystemwindowstestdrawer.largeImage.LargeImageTestActivity;
-import com.yuyang.fitsystemwindowstestdrawer.listViewHolder.ListTestActivity;
-import com.yuyang.fitsystemwindowstestdrawer.mediaPlayerAbout.MediaAboutActivity;
-import com.yuyang.fitsystemwindowstestdrawer.mvp.activity.UserInfoMvpTest;
-import com.yuyang.fitsystemwindowstestdrawer.recyclerViewSwipeRefresh.SwipeRefreshActivity;
 import com.yuyang.fitsystemwindowstestdrawer.service.BackgroundService;
-import com.yuyang.fitsystemwindowstestdrawer.sourceCodeAnalyze.SourceCodeAnalyzeActivity;
-import com.yuyang.fitsystemwindowstestdrawer.swipeBackActivity.SimpleSwipeBackActivity;
-import com.yuyang.fitsystemwindowstestdrawer.tantan.TantanActivity;
-import com.yuyang.fitsystemwindowstestdrawer.userDefinedWidget.UserDefinedWidgetActivity;
-import com.yuyang.fitsystemwindowstestdrawer.webview.WebViewActivity;
 
 /**
  * tableLayout 相关属性设定：
@@ -46,26 +27,7 @@ import com.yuyang.fitsystemwindowstestdrawer.webview.WebViewActivity;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Button button;
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
-    private Button button6;
-    private Button button7;
-    private Button button8;
-    private Button button9;
-    private Button button10;
-    private Button button11;
-    private Button button12;
-    private Button button13;
-    private Button button14;
-    private Button button15;
-    private Button button16;
-    private Button button17;
-    private Button button18;
-    private Button button19;
-    private Button button20;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,186 +59,11 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, OptionsCompatActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SourceCodeAnalyzeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MetricsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HorizontalFlingActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button5 = (Button) findViewById(R.id.button5);
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UserDefinedWidgetActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button6 = (Button) findViewById(R.id.button6);
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UserInfoMvpTest.class);
-                startActivity(intent);
-            }
-        });
-
-        button7 = (Button) findViewById(R.id.button7);
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ListTestActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button8 = (Button) findViewById(R.id.button8);
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LargeImageTestActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button9 = (Button) findViewById(R.id.button9);
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CardViewPagerActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button10 = (Button) findViewById(R.id.button10);
-        button10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TantanActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button11 = (Button) findViewById(R.id.button11);
-        button11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AnimationActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button12 = (Button) findViewById(R.id.button12);
-        button12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MaterialDesignActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button13 = (Button) findViewById(R.id.button13);
-        button13.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DestWidgetActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button14 = (Button) findViewById(R.id.button14);
-        button14.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button15 = (Button) findViewById(R.id.button15);
-        button15.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SwipeRefreshActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button16 = (Button) findViewById(R.id.button16);
-        button16.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SimpleSwipeBackActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button17 = (Button) findViewById(R.id.button17);
-        button17.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CustomActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button18 = (Button) findViewById(R.id.button18);
-        button18.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GoogleWidgetActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button19 = (Button) findViewById(R.id.button19);
-        button19.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MediaAboutActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button20 = (Button) findViewById(R.id.button20);
-        button20.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ConnectivityStateActivity.class);
-                startActivity(intent);
-            }
-        });
+        fragmentManager = getSupportFragmentManager();
+        MainFragment1 fragmet1 = new MainFragment1();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_content, fragmet1);
+        transaction.commit();
     }
 
     @Override
---
+++ app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/MainFragment1.java
@@ -0,0 +1,250 @@
+package com.yuyang.fitsystemwindowstestdrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.yuyang.fitsystemwindowstestdrawer.MetricsAbout.MetricsActivity;
+import com.yuyang.fitsystemwindowstestdrawer.androidLAnimationAbout.OptionsCompatActivity;
+import com.yuyang.fitsystemwindowstestdrawer.animationAbout.AnimationActivity;
+import com.yuyang.fitsystemwindowstestdrawer.cardViewPager.CardViewPagerActivity;
+import com.yuyang.fitsystemwindowstestdrawer.coordinatorLayoutAbout.MaterialDesignActivity;
+import com.yuyang.fitsystemwindowstestdrawer.customView.CustomActivity;
+import com.yuyang.fitsystemwindowstestdrawer.destWidget.DestWidgetActivity;
+import com.yuyang.fitsystemwindowstestdrawer.googleWidget.GoogleWidgetActivity;
+import com.yuyang.fitsystemwindowstestdrawer.horizontalFling.HorizontalFlingActivity;
+import com.yuyang.fitsystemwindowstestdrawer.internetAbout.manager.ConnectivityStateActivity;
+import com.yuyang.fitsystemwindowstestdrawer.largeImage.LargeImageTestActivity;
+import com.yuyang.fitsystemwindowstestdrawer.listViewHolder.ListTestActivity;
+import com.yuyang.fitsystemwindowstestdrawer.mediaPlayerAbout.MediaAboutActivity;
+import com.yuyang.fitsystemwindowstestdrawer.mvp.activity.UserInfoMvpTest;
+import com.yuyang.fitsystemwindowstestdrawer.recyclerViewSwipeRefresh.SwipeRefreshActivity;
+import com.yuyang.fitsystemwindowstestdrawer.sourceCodeAnalyze.SourceCodeAnalyzeActivity;
+import com.yuyang.fitsystemwindowstestdrawer.swipeBackActivity.SimpleSwipeBackActivity;
+import com.yuyang.fitsystemwindowstestdrawer.tantan.TantanActivity;
+import com.yuyang.fitsystemwindowstestdrawer.userDefinedWidget.UserDefinedWidgetActivity;
+import com.yuyang.fitsystemwindowstestdrawer.webview.WebViewActivity;
+
+/**
+ * Created by yuyang on 2016/6/2.
+ */
+public class MainFragment1 extends Fragment {
+    private View mContentView;
+
+    private Button button;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
+    private Button button11;
+    private Button button12;
+    private Button button13;
+    private Button button14;
+    private Button button15;
+    private Button button16;
+    private Button button17;
+    private Button button18;
+    private Button button19;
+    private Button button20;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mContentView = inflater.inflate(R.layout.fragment_main_1, container, false);
+        return mContentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        button = (Button) mContentView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), OptionsCompatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2 = (Button) mContentView.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SourceCodeAnalyzeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3 = (Button) mContentView.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MetricsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button4 = (Button) mContentView.findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), HorizontalFlingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button5 = (Button) mContentView.findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserDefinedWidgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button6 = (Button) mContentView.findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), UserInfoMvpTest.class);
+                startActivity(intent);
+            }
+        });
+
+        button7 = (Button) mContentView.findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ListTestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button8 = (Button) mContentView.findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), LargeImageTestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button9 = (Button) mContentView.findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), CardViewPagerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button10 = (Button) mContentView.findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), TantanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button11 = (Button) mContentView.findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AnimationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button12 = (Button) mContentView.findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MaterialDesignActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button13 = (Button) mContentView.findViewById(R.id.button13);
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DestWidgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button14 = (Button) mContentView.findViewById(R.id.button14);
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button15 = (Button) mContentView.findViewById(R.id.button15);
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SwipeRefreshActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button16 = (Button) mContentView.findViewById(R.id.button16);
+        button16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SimpleSwipeBackActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button17 = (Button) mContentView.findViewById(R.id.button17);
+        button17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), CustomActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button18 = (Button) mContentView.findViewById(R.id.button18);
+        button18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), GoogleWidgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button19 = (Button) mContentView.findViewById(R.id.button19);
+        button19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MediaAboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button20 = (Button) mContentView.findViewById(R.id.button20);
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ConnectivityStateActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
