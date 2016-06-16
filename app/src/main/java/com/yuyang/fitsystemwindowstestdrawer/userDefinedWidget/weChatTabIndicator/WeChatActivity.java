f8e20c68c42e5ec61696dbeab546a4f260c65e85
yuyang272999712 <272999712@qq.com>
2016-06-16 14:40:15 +0800

    放微信底部导航

--- app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/userDefinedWidget/weChatTabIndicator/WeChatActivity.java
+++ app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/userDefinedWidget/weChatTabIndicator/WeChatActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import com.yuyang.fitsystemwindowstestdrawer.R;
 import com.yuyang.fitsystemwindowstestdrawer.userDefinedWidget.weChatTabIndicator.ChangeColorIcon.ChangeColorIconWithTextView;
+import com.yuyang.fitsystemwindowstestdrawer.userDefinedWidget.weChatTabIndicator.ChangeColorLayout.BottomItemChangeColor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +26,8 @@ public class WeChatActivity extends AppCompatActivity implements ViewPager.OnPag
     private String[] mTitles = new String[] { "First Fragment!",
             "Second Fragment!", "Third Fragment!", "Fourth Fragment!" };
 
-    private List<ChangeColorIconWithTextView> mTabIndicator = new ArrayList<ChangeColorIconWithTextView>();
+    private List<ChangeColorIconWithTextView> mTabIndicator1 = new ArrayList<ChangeColorIconWithTextView>();
+    private List<BottomItemChangeColor> mTabIndicator2 = new ArrayList<BottomItemChangeColor>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -68,28 +70,45 @@ public class WeChatActivity extends AppCompatActivity implements ViewPager.OnPag
         ChangeColorIconWithTextView two = (ChangeColorIconWithTextView) findViewById(R.id.wechat_indicator_2);
         ChangeColorIconWithTextView three = (ChangeColorIconWithTextView) findViewById(R.id.wechat_indicator_3);
         ChangeColorIconWithTextView four = (ChangeColorIconWithTextView) findViewById(R.id.wechat_indicator_4);
-
-        mTabIndicator.add(one);
-        mTabIndicator.add(two);
-        mTabIndicator.add(three);
-        mTabIndicator.add(four);
-
+        mTabIndicator1.add(one);
+        mTabIndicator1.add(two);
+        mTabIndicator1.add(three);
+        mTabIndicator1.add(four);
         one.setOnClickListener(this);
         two.setOnClickListener(this);
         three.setOnClickListener(this);
         four.setOnClickListener(this);
-
+        //初始化第一个选中
         one.setIconAlpha(1.0f);
+
+        BottomItemChangeColor tab0 = (BottomItemChangeColor) findViewById(R.id.wechat_tab0);
+        BottomItemChangeColor tab1 = (BottomItemChangeColor) findViewById(R.id.wechat_tab1);
+        BottomItemChangeColor tab2 = (BottomItemChangeColor) findViewById(R.id.wechat_tab2);
+        BottomItemChangeColor tab3 = (BottomItemChangeColor) findViewById(R.id.wechat_tab3);
+        mTabIndicator2.add(tab0);
+        mTabIndicator2.add(tab1);
+        mTabIndicator2.add(tab2);
+        mTabIndicator2.add(tab3);
+        tab0.setOnClickListener(this);
+        tab1.setOnClickListener(this);
+        tab2.setOnClickListener(this);
+        tab3.setOnClickListener(this);
+        //初始化第一个选中
+        tab0.setIconAlpha(1);
     }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
         if (positionOffset > 0) {
-            ChangeColorIconWithTextView left = mTabIndicator.get(position);
-            ChangeColorIconWithTextView right = mTabIndicator.get(position + 1);
-
+            ChangeColorIconWithTextView left = mTabIndicator1.get(position);
+            ChangeColorIconWithTextView right = mTabIndicator1.get(position + 1);
             left.setIconAlpha(1 - positionOffset);
             right.setIconAlpha(positionOffset);
+
+            BottomItemChangeColor leftTab = mTabIndicator2.get(position);
+            BottomItemChangeColor rightTab = mTabIndicator2.get(position+1);
+            leftTab.setIconAlpha(1 - positionOffset);
+            rightTab.setIconAlpha(positionOffset);
         }
     }
 
@@ -108,28 +127,36 @@ public class WeChatActivity extends AppCompatActivity implements ViewPager.OnPag
         resetOtherTabs();
         switch (v.getId()) {
             case R.id.wechat_indicator_1:
-                mTabIndicator.get(0).setIconAlpha(1.0f);
+            case R.id.wechat_tab0:
+                mTabIndicator1.get(0).setIconAlpha(1.0f);
                 viewPager.setCurrentItem(0, false);
                 break;
             case R.id.wechat_indicator_2:
-                mTabIndicator.get(1).setIconAlpha(1.0f);
+            case R.id.wechat_tab1:
+                mTabIndicator1.get(1).setIconAlpha(1.0f);
                 viewPager.setCurrentItem(1, false);
                 break;
             case R.id.wechat_indicator_3:
-                mTabIndicator.get(2).setIconAlpha(1.0f);
+            case R.id.wechat_tab2:
+                mTabIndicator1.get(2).setIconAlpha(1.0f);
                 viewPager.setCurrentItem(2, false);
                 break;
             case R.id.wechat_indicator_4:
-                mTabIndicator.get(3).setIconAlpha(1.0f);
+            case R.id.wechat_tab3:
+                mTabIndicator1.get(3).setIconAlpha(1.0f);
                 viewPager.setCurrentItem(3, false);
                 break;
 
         }
     }
 
+    /**
+     * 重置所有tab状态
+     */
     private void resetOtherTabs() {
-        for (int i = 0; i < mTabIndicator.size(); i++) {
-            mTabIndicator.get(i).setIconAlpha(0);
+        for (int i = 0; i < mTabIndicator1.size(); i++) {
+            mTabIndicator1.get(i).setIconAlpha(0);
+            mTabIndicator2.get(i).setIconAlpha(0);
         }
     }
 }
