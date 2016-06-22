0449fe82fb5515c34829eec2b1cd45c12b4de277
yuyang272999712 <272999712@qq.com>
2016-06-22 18:34:24 +0800

    各种工具类

---
+++ app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/utils/ToastUtils.java
@@ -0,0 +1,72 @@
+package com.yuyang.fitsystemwindowstestdrawer.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Toast统一管理类，可以自定义Toast现实形式
+ */
+public class ToastUtils {
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, CharSequence message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, int message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, CharSequence message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, int message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, CharSequence message, int duration) {
+        Toast.makeText(context, message, duration).show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, int message, int duration) {
+        Toast.makeText(context, message, duration).show();
+    }
+}
