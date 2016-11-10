f5f3426cb1206657c0933efb80a3ec7745a88709
yuyang272999712 <272999712@qq.com>
2016-11-10 13:44:36 +0800

    雷达图

--- app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/userDefinedWidget/polygonsStatisticsView/RadarChartActivity.java
+++ app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/userDefinedWidget/polygonsStatisticsView/RadarChartActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.yuyang.fitsystemwindowstestdrawer.R;
 
@@ -12,8 +13,10 @@ import com.yuyang.fitsystemwindowstestdrawer.R;
  * 雷达图
  */
 
-public class RadarChartActivity extends AppCompatActivity {
+public class RadarChartActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     private Toolbar mToolbar;
+    private RadarChartView radarChartView;
+    private SeekBar seekBar1,seekBar2,seekBar3,seekBar4,seekBar5,seekBar6;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +24,22 @@ public class RadarChartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_radar_cahrt);
 
         setToolbar();
+
+        radarChartView = (RadarChartView) findViewById(R.id.radar_chart);
+        radarChartView.setDataValues(0.3f, 0.5f, 0.8f, 0.1f, 0.2f, 0.9f);
+
+        seekBar1 = (SeekBar) findViewById(R.id.value_1);
+        seekBar2 = (SeekBar) findViewById(R.id.value_2);
+        seekBar3 = (SeekBar) findViewById(R.id.value_3);
+        seekBar4 = (SeekBar) findViewById(R.id.value_4);
+        seekBar5 = (SeekBar) findViewById(R.id.value_5);
+        seekBar6 = (SeekBar) findViewById(R.id.value_6);
+        seekBar1.setOnSeekBarChangeListener(this);
+        seekBar2.setOnSeekBarChangeListener(this);
+        seekBar3.setOnSeekBarChangeListener(this);
+        seekBar4.setOnSeekBarChangeListener(this);
+        seekBar5.setOnSeekBarChangeListener(this);
+        seekBar6.setOnSeekBarChangeListener(this);
     }
 
     private void setToolbar() {
@@ -34,5 +53,40 @@ public class RadarChartActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        float percent = progress*1f/seekBar.getMax();
+        switch (seekBar.getId()){
+            case R.id.value_1:
+                radarChartView.setDataValue(percent, 0);
+                break;
+            case R.id.value_2:
+                radarChartView.setDataValue(percent, 1);
+                break;
+            case R.id.value_3:
+                radarChartView.setDataValue(percent, 2);
+                break;
+            case R.id.value_4:
+                radarChartView.setDataValue(percent, 3);
+                break;
+            case R.id.value_5:
+                radarChartView.setDataValue(percent, 4);
+                break;
+            case R.id.value_6:
+                radarChartView.setDataValue(percent, 5);
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
--- app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/userDefinedWidget/polygonsStatisticsView/RadarChartView.java
+++ app/src/main/java/com/yuyang/fitsystemwindowstestdrawer/userDefinedWidget/polygonsStatisticsView/RadarChartView.java
@@ -6,7 +6,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PathMeasure;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -25,31 +24,34 @@ import java.util.List;
  */
 
 public class RadarChartView extends View {
+    private final static String TAG = "RadarChartView";
     private final static int DEFAULT_TEXT_SIZE = 14;//默认文字大小sp
     private final static int DEFAULT_VIEW_LINE_WIDTH = 1;//默认网格线宽dp
     private final static int DEFAULT_VALUE_LINE_WIDTH = 3;//默认进度线宽dp
+    private final static int DEFAULT_STEP_COUNT = 4;//默认雷达图颜色阶梯数
 
     private Paint mPaint;
     private int mDataCount;//数据集个数
     private List<CharSequence> mDataNames = new ArrayList<>();//数据名称
+    private List<Float> mDataValues = new ArrayList<>();//数据对应的数值
     private float textSize;//文字大小
     private int textColor;//文字颜色
-    private int outerLayerColor;//最外层背景色（必须半透明，如果不是透明的那就强制转换为75%透明度）
+    private int stepLayerColor;//最外层背景色（必须半透明，如果不是透明的那就强制转换为75%透明度）
+    private int valueLinesColor;//进度线颜色
     private CharSequence[] dataNames;
     private int defaultSize = 300;//默认大小
     private int height;//View宽高
     private int width;
-    private int textHeight;//字符串宽高
+    private float textHeight;//字符串宽高
     private int textWidth;
+    private float textBottomHeight;//文字baseLine与文字最底部的距离
     private float radius;//半径
     private PointF centerPoint;//中心点
-    private float viewLineWidth = 3;//网格线宽度
+    private float viewLineWidth = 3;//分割线宽度
     private float valueLineWidth;//进度线宽度
-    private float angle;//夹角
-    private Path firstPath;//最外层圆
-    private Path secondPath;
-    private Path thirdPath;
-    private Path fourthPath;
+    private float includedAngle;//夹角
+    private int stepCount = DEFAULT_STEP_COUNT;
+    private Path path = new Path();
 
     public RadarChartView(Context context) {
         this(context, null);
@@ -64,35 +66,31 @@ public class RadarChartView extends View {
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.RadarChart);
         textColor = array.getColor(R.styleable.RadarChart_polygons_text_color, Color.BLACK);
         textSize = array.getDimension(R.styleable.RadarChart_polygons_text_size, DensityUtils.sp2px(context, DEFAULT_TEXT_SIZE));
-        outerLayerColor = array.getColor(R.styleable.RadarChart_polygons_bg_color, Color.parseColor("#2648afb6"));
-        if (outerLayerColor < 0x00ffffff){//如果所给的背景色是不透明的
-            outerLayerColor += 0x26000000;
+        stepLayerColor = array.getColor(R.styleable.RadarChart_polygons_bg_color, Color.parseColor("#2648afb6"));
+        if (stepLayerColor < 0x00ffffff){//如果所给的背景色是不透明的
+            stepLayerColor += 0x26000000;
         }
+        valueLinesColor = array.getColor(R.styleable.RadarChart_polygons_line_color, Color.RED);
         viewLineWidth = array.getDimension(R.styleable.RadarChart_polygons_view_line_width, DensityUtils.dp2px(context, DEFAULT_VIEW_LINE_WIDTH));
         valueLineWidth = array.getDimension(R.styleable.RadarChart_polygons_value_line_width, DensityUtils.dp2px(context, DEFAULT_VALUE_LINE_WIDTH));
         dataNames = array.getTextArray(R.styleable.RadarChart_polygons_datas);
         if (dataNames == null || dataNames.length == 0){
-            Log.e("PolygonsStatisticsView", "必须设定要显示的数据项");
+            Log.e(TAG, "必须设定要显示的数据项");
             throw new RuntimeException("必须设定要显示的数据项");
         }else {
             mDataNames.addAll(Arrays.asList(dataNames));
             mDataCount = mDataNames.size();
-            angle = 360f/mDataCount;
+            includedAngle = 360f/mDataCount;
+            for (int i=0; i<mDataCount; i++){//初始化进度值
+                mDataValues.add(0f);
+            }
         }
-        /*if (dataNameResourceId > 0){
-            mDataNames.addAll(Arrays.asList(context.getResources().getStringArray(dataNameResourceId)));
-            mDataCount = mDataNames.size();
-            angle = 360f/mDataCount;
-        }else {
-            Log.e("PolygonsStatisticsView", "必须设定要显示的数据项");
-            throw new RuntimeException("必须设定要显示的数据项");
-        }*/
         if (mDataCount < 3){
-            Log.e("PolygonsStatisticsView", "dataNameResourceId对应的数据项不能少于3个");
+            Log.e(TAG, "dataNameResourceId对应的数据项不能少于3个");
             throw new RuntimeException("dataNameResourceId对应的数据项不能少于3个");
         }
         if (mDataNames.size() > 8){
-            Log.e("PolygonsStatisticsView", "数据项太多了吧，我猜绘制出来肯定不好看");
+            Log.w(TAG, "数据项太多了吧，我猜绘制出来肯定不好看");
         }
         array.recycle();
 
@@ -100,6 +98,9 @@ public class RadarChartView extends View {
 
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setTextSize(textSize);
+        Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
+        textHeight = fontMetrics.bottom-fontMetrics.top;
+        textBottomHeight = fontMetrics.bottom;
 
         Rect textRect = new Rect();
         //获取最长的字符串
@@ -112,24 +113,8 @@ public class RadarChartView extends View {
             }
         }
         mPaint.getTextBounds(mDataNames.get(position).toString(), 0, mDataNames.get(position).length(), textRect);
-        textHeight = textRect.height();
         textWidth = textRect.width();
     }
-
-    /**
-     * 添加数据点个数
-     */
-    /*public void setDataNames(String... names){
-        setDataNames(Arrays.asList(names));
-    }
-
-    public void setDataNames(Collection<? extends String> names){
-        if (names != null){
-            mDataNames.addAll(names);
-            mDataCount = mDataNames.size();
-            invalidate();
-        }
-    }*/
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -140,7 +125,7 @@ public class RadarChartView extends View {
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
 
         if (heightMode != MeasureSpec.EXACTLY){
-            height = Math.min(height, defaultSize+textHeight*2);
+            height = (int) (Math.min(height, defaultSize+textHeight*2)+0.5);
         }
         if (widthMode != MeasureSpec.EXACTLY){
             width = Math.min(width, defaultSize+textWidth*2);
@@ -148,27 +133,62 @@ public class RadarChartView extends View {
         radius = Math.min(height/2-textHeight, width/2-textWidth);//半径
         centerPoint = new PointF(width/2, height/2);//中心点
 
-        firstPath = new Path();
-        firstPath.moveTo(width/2, height/2-radius);
-        firstPath.addCircle(width/2, height/2, radius, Path.Direction.CW);
-        secondPath = new Path();
-        secondPath.moveTo(width/2, height/2-radius*3/4);
-        secondPath.addCircle(width/2, height/2, radius*3/4, Path.Direction.CW);
-        thirdPath = new Path();
-        thirdPath.moveTo(width/2, height/2-radius/2);
-        thirdPath.addCircle(width/2, height/2, radius/2, Path.Direction.CW);
-        fourthPath = new Path();
-        fourthPath.moveTo(width/2, height/2-radius/4);
-        fourthPath.addCircle(width/2, height/2, radius/4, Path.Direction.CW);
-
         setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawCenterLines(canvas);
         drawDataNames(canvas);
+        drawRadarChart(canvas);
+        drawCenterLines(canvas);
+        drawValueLines(canvas);
+    }
+
+    /**
+     * 绘制进度线（各个数据的实际值连线）
+     * @param canvas
+     */
+    private void drawValueLines(Canvas canvas) {
+        mPaint.setColor(valueLinesColor);
+        mPaint.setStrokeWidth(valueLineWidth);
+        mPaint.setStyle(Paint.Style.STROKE);
+        path.reset();
+        float radius = this.radius*mDataValues.get(0);
+        float angle = 0*includedAngle;
+        float[] vertex = getXYByAngleAndRadius(angle, radius);
+        path.moveTo(vertex[0], vertex[1]);//第一个点
+        for (int i=1; i<mDataCount; i++){
+            angle = i*includedAngle;
+            radius = this.radius*mDataValues.get(i);
+            vertex = getXYByAngleAndRadius(angle, radius);
+            path.lineTo(vertex[0], vertex[1]);//第一个点
+        }
+        path.close();
+        canvas.drawPath(path, mPaint);
+    }
+
+    /**
+     * 画雷达图背景
+     * @param canvas
+     */
+    private void drawRadarChart(Canvas canvas) {
+        mPaint.setColor(stepLayerColor);
+        mPaint.setStyle(Paint.Style.FILL);
+        float[] vertex;
+        for (int i=0; i<stepCount; i++){//画背景的阶梯渐变色
+            path.reset();
+            float radius = this.radius*(stepCount-i)/stepCount;
+            vertex = getXYByAngleAndRadius(0, radius);
+            path.moveTo(vertex[0], vertex[1]);
+            for (int j=1; j<mDataCount; j++){
+                float angle = j*includedAngle;
+                vertex = getXYByAngleAndRadius(angle, radius);
+                path.lineTo(vertex[0], vertex[1]);
+            }
+            path.close();
+            canvas.drawPath(path, mPaint);
+        }
     }
 
     /**
@@ -177,20 +197,26 @@ public class RadarChartView extends View {
      */
     private void drawDataNames(Canvas canvas) {
         mPaint.setColor(textColor);
-        PathMeasure pathMeasure = new PathMeasure(firstPath, true);
-        float pathLength = pathMeasure.getLength();
+        mPaint.setStyle(Paint.Style.FILL);
         for (int i=0; i<mDataCount; i++){
-            float[] textPosition = new float[2];
-            pathMeasure.getPosTan(pathLength/mDataCount*i, textPosition, null);
-            if (i == 0){//第一个点，文字肯定绘制在最上面
-                canvas.drawText(mDataNames.get(i).toString(), centerPoint.x-textWidth/2, centerPoint.y-radius, mPaint);
-            }else if (mDataCount%2==0 && i==mDataCount/2){//整除说明最下面正中需要绘制文字
-                canvas.drawText(mDataNames.get(i).toString(), centerPoint.x-textWidth/2, centerPoint.y+radius+textHeight, mPaint);
-            }else if (mDataCount*1.0f/2>i){//右侧文字
-                canvas.drawText(mDataNames.get(i).toString(), textPosition[0], textPosition[1]-textHeight/2, mPaint);
-            }else {//左侧文字
-                canvas.drawText(mDataNames.get(i).toString(), textPosition[0]+textWidth, textPosition[1]-textHeight/2, mPaint);
+            float angle = i*includedAngle;
+            String name = mDataNames.get(i).toString();
+            float textWidth = mPaint.measureText(name);
+            float x = 0,y = 0;//文字起始坐标
+            if (angle == 0){//第一条竖线
+                x = centerPoint.x-textWidth/2;
+                y = textHeight;
+            }else if (angle>0 && angle<180){//在第一四象限，文字显示在右面
+                x = centerPoint.x + (float) (radius*Math.sin(Math.toRadians(angle)));
+                y = centerPoint.y - (float) (radius*Math.cos(Math.toRadians(angle))) + textHeight/2;
+            }else if (angle == 180){//竖直向下的线
+                x = centerPoint.x-textWidth/2;
+                y = centerPoint.y+radius+textHeight;
+            }else {//第二三象限，文字显示在左面
+                x = centerPoint.x + (float) (radius*Math.sin(Math.toRadians(angle))) - textWidth;
+                y = centerPoint.y - (float) (radius*Math.cos(Math.toRadians(angle))) + textHeight/2;
             }
+            canvas.drawText(name, x, y-textBottomHeight, mPaint);
         }
     }
 
@@ -198,13 +224,66 @@ public class RadarChartView extends View {
      * 画中心星形线
      */
     private void drawCenterLines(Canvas canvas) {
-        mPaint.setColor(Color.BLACK);
+        mPaint.setColor(Color.WHITE);
         mPaint.setStrokeWidth(viewLineWidth);
+        mPaint.setStyle(Paint.Style.FILL);
         canvas.save();
         for (int i=0; i<mDataCount; i++){
             canvas.drawLine(centerPoint.x, centerPoint.y, centerPoint.x, centerPoint.y-radius, mPaint);
-            canvas.rotate(angle, centerPoint.x, centerPoint.y);
+            canvas.rotate(includedAngle, centerPoint.x, centerPoint.y);
         }
         canvas.restore();
     }
+
+    /**
+     * 通过角度和半径获取顶点坐标
+     * @param angle
+     * @param radius
+     * @return
+     */
+    private float[] getXYByAngleAndRadius(float angle, float radius){
+        float x = 0,y = 0;//顶点坐标
+        if (angle == 0){//第一条竖线
+            x = centerPoint.x;
+            y = centerPoint.y - radius;
+        }else if (angle>0 && angle<180){//在第一四象限
+            x = centerPoint.x + (float) (radius*Math.sin(Math.toRadians(angle)));
+            y = centerPoint.y - (float) (radius*Math.cos(Math.toRadians(angle)));
+        }else if (angle == 180){//竖直向下的线
+            x = centerPoint.x;
+            y = centerPoint.y + radius;
+        }else {//第二三象限
+            x = centerPoint.x + (float) (radius*Math.sin(Math.toRadians(angle)));
+            y = centerPoint.y - (float) (radius*Math.cos(Math.toRadians(angle)));
+        }
+        return new float[]{x, y};
+    }
+
+    /**供外部调用的方法********************************************/
+    public void setDataValues(Float... values){
+        setDataValues(Arrays.asList(values));
+    }
+
+    public void setDataValues(List<Float> values){
+        if (values.size() != mDataCount){
+            Log.w(TAG, "数据无法实现一一对应关系！");
+            return;
+        }
+        if (values != null){
+            mDataValues.clear();
+            mDataValues.addAll(values);
+            postInvalidate();
+        }
+    }
+
+    public void setDataValue(float value, int index){
+        if (index >= mDataCount){
+            Log.w(TAG, "数据无法实现一一对应关系！");
+            return;
+        }
+        mDataValues.remove(index);
+        mDataValues.add(index, value);
+        postInvalidate();
+    }
+
 }
